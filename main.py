73e4ca7439f15e9a36b00a20d3fd23f4b420aeaf
MrPixel555 <153556869+MrPixel555@users.noreply.github.com>
2026-03-11 03:00:26 +0330

    Implement infinite sun hook in Plants Vs Zombies
    Refactored memory manipulation code to install an infinite sun hook for the Plants Vs Zombies game.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pymem
+import pymem.process
+import struct
+
 pm = pymem.Pymem("PlantsVsZombies.exe")
-address = 0x1D25A578
-change_value = 9999
-pm.write_int(address,change_value)
+
+module = pymem.process.module_from_name(pm.process_handle, "PlantsVsZombies.exe")
+base = module.lpBaseOfDll
+
+instr = base + 0x94445
+
+newmem = pm.allocate(1024)
+
+jmp = newmem - instr - 5
+pm.write_bytes(instr, b'\xE9' + struct.pack('<i', jmp), 5)
+
+pm.write_bytes(instr + 5, b'\x90', 1)
+
+code = b''
+
+code += b'\xC7\x87\x78\x55\x00\x00' + struct.pack('<I',9999)
+
+code += b'\x8B\x87\x78\x55\x00\x00'
+
+jmp_back = (instr + 6) - (newmem + len(code) + 5)
+code += b'\xE9' + struct.pack('<i', jmp_back)
+
+pm.write_bytes(newmem, code, len(code))
+
+print("Infinite Sun hook installed")
